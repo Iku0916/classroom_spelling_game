482c17b2529b8c067a445f1d0394b992cda2b83c
Iku <ikufullmoon19@gmail.com>
2025-12-30 13:17:46 +0900

    ADD:ゲーム参加者待機画面の作成（続き）

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user || create_guest
+    end
+
+    private
+
+      def find_verified_user
+        User.find_by(id: cookies.encrypted[:user_id])
+      end
+
+      def create_guest
+        guest = Guest.find_by(id: cookies.encrypted[:guest_id])
   end
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import TimerController from "./timer_controller"
 
 const application = Application.start()
+
+application.debug = false
 window.Stimulus = application
 
 export { application }
