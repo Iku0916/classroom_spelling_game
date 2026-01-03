b7f320c72d02b1670bc053efe3525af4c1533aab
Iku <ikufullmoon19@gmail.com>
2026-01-03 10:38:30 +0900

    ADD:正誤判断のJSの実装（正解＋１　不正解−１）

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,10 @@ import HelloController from "./hello_controller"
 import ModalController from "./modal_controller"
 import TimerController from "./timer_controller"
 import GameController from "./game_controller"
+import ScoreController from "./score_controller"
 
 application.register("hello", HelloController)
 application.register("modal", ModalController)
 application.register("timer", TimerController)
 application.register("game", GameController)
+application.register("score", ScoreController)
