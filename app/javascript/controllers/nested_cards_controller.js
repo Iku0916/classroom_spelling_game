e222c981fcce366c53485b39006800d1d1630968
Iku <ikufullmoon19@gmail.com>
2026-01-16 13:50:48 +0900

    Fix conflict and keep word_kits.css

--- app/javascript/controllers/nested_cards_controller.js
+++ app/javascript/controllers/nested_cards_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["container"]
 
-  add() {
+  add(event) {
+    event.preventDefault()
     const time = new Date().getTime()
 
     const html = `
@@ -11,19 +12,25 @@ export default class extends Controller {
         <input type="text"
                name="word_kit[word_cards_attributes][${time}][english_word]"
                placeholder="英単語"
-               class="english-input">
+               class="input-field">
 
         <input type="text"
                name="word_kit[word_cards_attributes][${time}][japanese_translation]"
                placeholder="日本語"
-               class="japanese-input">
+               class="input-field">
+
+        <label style="color: #e74c3c; font-size: 12px; min-width: 40px; text-align: center;">
+          削除<br>
+          <input type="checkbox" 
+                 name="word_kit[word_cards_attributes][${time}][_destroy]" 
+                 value="1">
+        </label>
       </div>
     `
 
     this.containerTarget.insertAdjacentHTML("beforeend", html)
 
     const lastCard = this.containerTarget.lastElementChild
-
     lastCard.scrollIntoView({ behavior: "smooth", block: "center" })
 
     const englishInput = lastCard.querySelector(".english-input")
