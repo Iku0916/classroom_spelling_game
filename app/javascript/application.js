6b55f4b7a45d0b329dca83ced7e9b6d98096f922
Iku <ikufullmoon19@gmail.com>
2026-01-20 13:04:58 +0900

    FIX:JS読み込みのため、importmapとapplication.jsの表記を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import "./channels/consumer"
-import "./channels/game_channel"
-import "./game_room"
+import "channels/consumer"
+import "channels/game_channel"
+import "game_room"
