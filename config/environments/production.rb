322f641020584fc7851a880f5a0251b526c51ef5
Iku <ikufullmoon19@gmail.com>
2026-01-23 13:06:36 +0900

    FIX:環境をDockerにして再デプロイ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "vocano-spelling-game.onrender.com"
+  config.hosts << "vocano-spelling-game-ver2.onrender.com"
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
