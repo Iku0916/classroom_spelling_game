d20637ece66f0eb213cdb0b618e3eb8d6fd93e3f
Iku <ikufullmoon19@gmail.com>
2026-02-04 10:51:02 +0900

    ADD:自習モードのコントローラを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_08_015644) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_04_011653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_08_015644) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_score"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
