78eaf931c8383ca57cdeef0ddc5ed878c34949bd
Iku <ikufullmoon19@gmail.com>
2026-02-27 11:16:12 +0900

    ADD:トークンの制限時間を設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/create'
-  get 'password_resets/edit'
-  get 'password_resets/update'
   get 'community/index'
   get "community/:id", to: "community#show", as: :community_kit
   get "onboardings", to: "onboardings#index"
