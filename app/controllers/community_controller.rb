c4b456be9a3808bb8fd3bdc5fd8e2ea32018de77
Iku <ikufullmoon19@gmail.com>
2026-03-02 09:56:17 +0900

    ADD:ゲームキット一覧とコミュニティー画面に検索機能を追加

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,6 +1,13 @@
 class CommunityController < ApplicationController
   def index
     @public_word_kits = WordKit.where(visibility: "public_kit")
+
+    if params[:keyword].present?
+      @public_word_kits = @public_word_kits.left_outer_joins(:tags).where(
+        "word_kits.name LIKE :q OR tags.name LIKE :q",
+        q: "%#{params[:keyword]}%"
+      ).distinct
+    end
   end
 
   def show
