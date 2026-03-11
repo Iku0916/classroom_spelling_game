9c3c37a7947bc7b927e31119c66ffaaf1df4217b
Iku <ikufullmoon19@gmail.com>
2026-03-11 10:27:57 +0900

    FIX:oauth_controllerのリファクタリング

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,23 +9,16 @@ class OauthsController < ApplicationController
 
   def callback
     provider = params[:provider]
-    Rails.logger.info '🔥 CALLBACK START'
 
     if (@user = login_from(provider))
-      Rails.logger.info '🔥 login_from success'
       redirect_to root_path, notice: "#{provider.titleize}でログインしました"
     else
-      Rails.logger.info '🔥 login_from failed'
-
       @user = create_from(provider)
-      Rails.logger.info "🔥 create_from result: #{@user.inspect}"
 
       if @user.persisted?
         auto_login(@user)
-        Rails.logger.info '🔥 auto_login done'
         redirect_to root_path, notice: "#{provider.titleize}でログインしました"
       else
-        Rails.logger.info '🔥 user not persisted'
         redirect_to login_path, alert: '保存失敗'
       end
     end
