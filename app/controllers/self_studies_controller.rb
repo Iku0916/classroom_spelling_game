ca3a11c36e41b47e246ae9011e9922042da3ddf3
Iku <ikufullmoon19@gmail.com>
2026-03-12 09:38:44 +0900

    FIX:self_study_controllerのリファクタリング

--- app/controllers/self_studies_controller.rb
+++ app/controllers/self_studies_controller.rb
@@ -1,56 +1,29 @@
 # frozen_string_literal: true
 
 class SelfStudiesController < ApplicationController
-  def new
-    @word_kit = WordKit.find(params[:word_kit_id])
-  end
+  before_action :set_word_kit, only: %i[new play answer update result]
+
+  def new; end
 
   def play
-    @word_kit = WordKit.find_by(id: params[:word_kit_id])
-    minutes = params[:time_limit_minutes].to_f
-    @time_limit = (minutes.positive? ? minutes : 1) * 60
-
-    @questions = @word_kit.word_cards.map do |word|
-      {
-        word: word.english_word,
-        correct_answer: word.japanese_translation
-      }
-    end
-
-    if @word_kit.nil?
-      redirect_to word_kits_path, alert: 'ゲームキットが見つかりませんでした'
-      return
-    end
-
-    @word_cards = @word_kit.word_cards
-    @total_questions = @word_cards.count
+    @time_limit = calculate_time_limit(params[:time_limit_minutes])
+    @questions = format_questions(@word_kit)
+    @total_questions = @word_kit.word_cards.count
   end
 
   def answer
-    @word_kit = WordKit.find(params[:word_kit_id])
     cards = @word_kit.word_cards.to_a
+    index = (session[:question_index] || 0) % cards.length
 
-    index = session[:question_index] || 0
-    index %= cards.length
-    current_card = cards[index]
-
-    session[:current_score] = (session[:current_score] || 0) + 1 if params[:answer] == current_card.japanese_translation
+    update_session_score(cards[index])
 
     session[:question_index] = index + 1
     head :ok
   end
 
   def update
-    @word_kit = WordKit.find(params[:word_kit_id])
     log_data = learning_log_params
-    score = log_data[:score].to_i
-    current_user.increment!(:total_score, score)
-
-    current_user.learning_logs.create!(
-      score: log_data[:score],
-      minutes: log_data[:minutes],
-      word_kit_id: @word_kit.id
-    )
+    save_learning_log(log_data)
 
     render json: { status: 'success' }, status: :ok
   rescue StandardError => e
@@ -58,16 +31,51 @@ class SelfStudiesController < ApplicationController
   end
 
   def result
-    @word_kit = WordKit.find(params[:word_kit_id])
     @score = params[:score].to_i
     @total = @word_kit.word_cards.count
     @user = current_user
+    reset_session_stats
+  end
 
+  private
+
+  def set_word_kit
+    @word_kit = WordKit.find(params[:word_kit_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to word_kits_path, alert: 'ゲームキットが見つかりませんでした'
+  end
+
+  def calculate_time_limit(minutes_param)
+    minutes = minutes_param.to_f
+    (minutes.positive? ? minutes : 1) * 60
+  end
+
+  def format_questions(word_kit)
+    word_kit.word_cards.map do |word|
+      { word: word.english_word, correct_answer: word.japanese_translation }
+    end
+  end
+
+  def update_session_score(current_card)
+    return unless params[:answer] == current_card.japanese_translation
+
+    session[:current_score] = (session[:current_score] || 0) + 1
+  end
+
+  def save_learning_log(log_data)
+    score = log_data[:score].to_i
+    current_user.increment!(:total_score, score)
+    current_user.learning_logs.create!(
+      log_data.merge(word_kit_id: @word_kit.id)
+    )
+  end
+
+  def reset_session_stats
     session[:current_score] = 0
     session[:question_index] = 0
   end
-end
 
-def learning_log_params
-  params.require(:learning_log).permit(:score, :minutes)
+  def learning_log_params
+    params.require(:learning_log).permit(:score, :minutes)
+  end
 end
