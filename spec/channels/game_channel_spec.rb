4ae84f47e845e60fee8f3350fe0b1e1246683027
Iku <ikufullmoon19@gmail.com>
2026-03-18 18:12:18 +0900

    ADD:チャンネルの接続・切断に関するテストコードの追加

---
+++ spec/channels/game_channel_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe GameChannel, type: :channel do
+  let(:user) do
+    User.create!(
+      name: 'イクちゃん',
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
+
+  let(:word_kit) { WordKit.create!(name: 'テストキット', user: user) }
+
+  let(:game_room) do
+    GameRoom.create!(
+      word_kit: word_kit,
+      host_user: user,
+      status: :waiting,
+      game_code: '123456',
+      time_limit: 300
+    )
+  end
+
+  describe 'チャンネル接続' do
+    it '正しいストリームにサブスクライブされること' do
+      subscribe(game_room_id: game_room.id)
+      expect(subscription).to be_confirmed
+      expect(subscription.streams).to include("game_channel_#{game_room.id}")
+    end
+  end
+
+  describe 'チャンネル切断' do
+    it 'エラーなく切断できること' do
+      subscribe(game_room_id: game_room.id)
+      expect { unsubscribe }.not_to raise_error
+    end
+  end
+end
